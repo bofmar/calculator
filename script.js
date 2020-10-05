d53500aef28ba6139559df0c6418b76bea5ae3dc
bofmar <bofmar@outlook.com>
2020-10-05 15:01:27 +0300

    added point and three operations

--- script.js
+++ script.js
@@ -2,18 +2,23 @@ const keys = document.querySelectorAll("button");
 const display = document.querySelector(".display");
 
 let currentDisplay = "0";
-let storedNumber = 0;
 let currentOperation = "";
+let nextOperation = "";
+
+let awaitNumber = false;
+
+let num1;
+let num2;
 
 display.textContent = currentDisplay;
 
 keys.forEach(key => {
-    key.addEventListener("click",getKeyID);
+    key.addEventListener("click", getKeyID);
 })
 
-function getKeyID(e){
+function getKeyID(e) {
     const keyId = e.target.id;
-    switch(keyId){
+    switch (keyId) {
         //number buttons
         case "zero":
             updateDisplay("0");
@@ -56,46 +61,116 @@ function getKeyID(e){
         case "plus":
             operate("plus");
             break;
+        case "minus":
+            operate("minus");
+            break;
+        case "divide":
+            operate("divide");
+            break;
+        case "multiply":
+            operate("multiply");
+            break;
+        case "power":
+            operate("power");
+            break;
+        //decimal
+        case "point":
+            updateDisplay(".");
+            break;
         default:
             console.log("something else was pressed");
     }
 }
 
-function updateDisplay(str){
+function updateDisplay(str) {
     //max length of display = 10 chars
-    (currentDisplay === "0" && str === "0")? 1 : (currentDisplay === "0" && str != "AC" && str != "DEL")? currentDisplay = str:(currentDisplay.length + 1 > 10 && str != "AC" && str != "DEL")? currentDisplay = "ERROR":(currentDisplay === "ERROR" && "AC")? 1:(str === "AC")? allClear():(str === "DEL")? removeADigit(): currentDisplay += str;
+    (currentDisplay === "0" && str === "0") ? 1 : (currentDisplay === "ERROR" && str != "AC") ? 1 : (str === "." && currentDisplay.includes(str)) ? 1 : (currentDisplay === "0" && str === ".") ? currentDisplay += str : (currentDisplay === "0" && str != "AC" && str != "DEL") ? currentDisplay = str : (currentDisplay.length + str.length > 10 && str != "AC" && str != "DEL") ? currentDisplay = "ERROR" : (str === "AC") ? allClear() : (str === "DEL") ? removeADigit() : currentDisplay += str;
 
     display.textContent = currentDisplay;
+    awaitNumber = false;
 }
 
-function removeADigit(){
+function removeADigit() {
     let displayToArray = [...currentDisplay];
-    currentDisplay = displayToArray.slice(0,displayToArray.length-1).join("");
-    if(currentDisplay.length < 1){
+    currentDisplay = displayToArray.slice(0, displayToArray.length - 1).join("");
+    if (currentDisplay.length < 1) {
         currentDisplay = "0";
     }
 }
 
-function allClear(){
+function allClear() {
     currentDisplay = "0";
     storedNumber = 0;
     currentOperation = "";
 }
 
-function operate(operator){
-    currentOperation = operator;
-    (operator === "plus")? addition(): 1;
-}
-
-function addition(){
-    if(currentDisplay === "ERROR"){
+function operate(operator) {
+    if (currentDisplay === "ERROR") {
         return;
     }
-    let result = +storedNumber + +currentDisplay;
-    storedNumber = currentDisplay;
-    currentDisplay = result.toString();
+    if (awaitNumber) {
+        currentOperation = operator;
+    }
+    else if (currentOperation === "") {
+        currentOperation = operator;
+        num1 = +currentDisplay;
+        awaitNumber = true;
+        currentDisplay = "0";
+        return;
+    }
+    else {
+        num2 = +currentDisplay;
+        nextOperation = operator;
 
-    console.log([result,storedNumber,currentDisplay]);
+        switch (currentOperation) {
+            case "plus":
+                add(num1, num2);
+                break;
+            case "minus":
+                subtract(num1,num2);
+                break;
+            case "divide":
+                divide(num1,num2);
+                break;
+            case "multiply":
+                multiply(num1,num2);
+                break;
+            case "power":
+                power(num1,num2);
+                break;
+        }
+    }
+}
 
-    display.textContent = currentDisplay;
+function add(x, y) {
+    let result = x + y;
+    reset(result);
+}
+
+function subtract(x,y){
+    let result = x - y;
+    reset(result);
+}
+
+function multiply(x,y){
+    let result = x * y;
+    reset(result);
+}
+
+function reset(result) {
+    if (result.toString().length > 10) {
+        currentDisplay = "ERROR";
+        updateDisplay("ERROR");
+        return;
+    }
+    awaitNumber = true;
+    num1 = result;
+    currentOperation = nextOperation;
+    nextOperation = "";
+    num2 = 0;
+    currentDisplay = "0";
+
+    updateDisplay(result.toString());
+
+    currentDisplay = "0";
 }
